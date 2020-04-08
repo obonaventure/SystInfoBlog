008a59d3bbc5ef82d1989f5b1a9d996e5877e2ed
Olivier Bonaventure <Olivier.Bonaventure@uclouvain.be>
2020-04-08 15:14:33 +0200

    Validation de l'orthographe avec travis

--- travis.sh
+++ travis.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mdspell --ignore-numbers --ignore-acronyms --report --no-suggestions --dictionary dicts/index README.md _posts/*.md
-jekyll build .
+mdspell --ignore-numbers --ignore-acronyms --report --no-suggestions --dictionary dicts/index README.md _posts/*.md && jekyll build .
